70a3136f4175ba1edbbc4f1b61406fb3f90030b4
mizanur2734 <mizanurbb2791@gmail.com>
2026-03-05 16:32:14 +0600

    added navbar with logo and coin button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-
+import navImg from "./assets/logo.png";
+import dollorImg from "./assets/dollor.png";
 function App() {
-
-
   return (
     <>
-      <h1>I am here .....</h1>
+      <div className="navbar w-full max-w-[1200px] mx-auto">
+        <div className="flex-1">
+          <img className="w-14 h-14" src={navImg} alt="" />
+        </div>
+
+        <div className="flex items-center">
+          <button className="btn border-2 border-blue-100 flex items-center">
+            <span className="mr-1">600000000</span>
+            <span className="mr-2">Coin</span>
+            <img className="w-6 h-6" src={dollorImg} alt="" />
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
