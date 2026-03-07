a8dfb0f4a5855200b84a7a67f102b420ee129037
mizanur2734 <mizanurbb2791@gmail.com>
2026-03-07 23:41:21 +0600

    add SelectedCard component to show selected player details

---
+++ src/components/SelectedCard/SelectedCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SelectedCard = ({ player }) => {
+  console.log(player);
+  return (
+    <div className="border border-gray-300 rounded-xl p-2 mt-2 ">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <img
+            src={player.player_image}
+            className="w-14 h-14 rounded-xl"
+            alt=""
+          />
+          <div className="ml-2">
+            <h3 className="text-lg ">{player.player_name}</h3>
+            <p className="text-xs">{player.player_role}</p>
+          </div>
+        </div>
+        <div>
+          <img src="https://iili.io/qzNnVDu.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SelectedCard;
--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import SelectedCard from '../SelectedCard/SelectedCard';
 
 const SelectedPlayers = ({purchasedPlayers}) => {
-    console.log(purchasedPlayers)
+    // console.log(purchasedPlayers)
     return (
-        <div>
-          <h1>selected</h1>
-        </div>
+       <div className='md:my-6 my-4'>
+        {
+            purchasedPlayers.map(player => <SelectedCard player={player}></SelectedCard>)
+        }
+       </div>
     );
 };
 
